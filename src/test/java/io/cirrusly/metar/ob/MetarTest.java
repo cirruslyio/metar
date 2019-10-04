01f41e59f307f64ed96c7d7a03f45d169762d388
Luke Campbell <luke.s.campbell@gmail.com>
2019-10-04 17:51:34 -0400

    Adds unit tests
    This commit adds JUnit to the project for unit testing.

---
+++ src/test/java/io/cirrusly/metar/ob/MetarTest.java
@@ -0,0 +1,14 @@
+package io.cirrusly.metar.ob;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class MetarTest {
+
+  @Test
+  public void testJunit() {
+    assertTrue(true);
+  }
+
+}
